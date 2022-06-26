9a5dc39773df2af373034acab07486562f9c036a
Aarni <dabeastelele@gmail.com>
2022-06-26 06:48:59 +0300

    osa3 tehty

--- index.js
+++ index.js
@@ -11,46 +11,58 @@ app.use(express.json())
 morgan.token('content', (req, res) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content '))
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-app.get('/api/persons', (request, response) => {
+  next(error)
+}
+
+app.get('/api/persons', (request, response, next) => {
   contacts.find({})
     .then(data => response.json(data))
+    .catch(error => next(error))
 })
 
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   contacts.find({})
     .then(data => {
       const len = data.length
       const now = new Date()
       const responseString = `Phonebook has info for ${len} poeple</br>${now}`
       response.send(responseString)
+    .catch(error => next(error))
     }
   )
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   contacts.findById(request.params.id)
     .then(contact => {
       response.json(contact)
     })
+    .catch(error => next(error))
   })
 
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   contacts.findByIdAndRemove(request.params.id).then(response.status(204).end())
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   if (!request.body.name || !request.body.number) {
     return response.status(400).json({error: "All data fields weren't filled"})
   }
-  /*else if (contacts.find(contact => contact.name === request.body.name)) {
-    return response.status(400).json({error: "The name is already in the contacts"})
-  }
-  */
+  
   const contact = new contacts({
     name: request.body.name,
     number: request.body.number
@@ -59,9 +71,22 @@ app.post('/api/persons', (request, response) => {
     .then(savedContact => {
       response.json(savedContact)
     })
+    .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const contact = {
+    name: body.name,
+    number: body.number
+  }
+  contacts.findByIdAndUpdate(body.id, contact, {new: true})
+  .then(updatedContact => response.json(updatedContact))
+  .catch(error => next(error))
+})
 
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
--- models/contact.js
+++ models/contact.js
@@ -9,7 +9,26 @@ mongoose.connect(process.env.DB_URL)
     .catch((error) => console.log('error detectedi: ', error.message))
 
 
-const noteSchema = new mongoose.Schema({name: String, number: String})
+const validator = (val) => {
+    if (val.length >= 8){
+        console.log('is length 8')
+        console.log('val[2] ===  "-": ',val[2] ===  "-", '      !isNaN(val.slice(0, 2)): ', !isNaN(val.slice(0, 2)), '    !isNaN(val.slice(3)): ', !isNaN(val.slice(3)))
+        if (val[2] ===  "-" && !isNaN(val.slice(0, 2)) && !isNaN(val.slice(3))) {
+            return true
+        }
+        else if (val[3] ===  "-" && !isNaN(val.slice(0, 3)) && !isNaN(val.slice(4))) {
+            return true
+        }
+    }
+    return false
+}
+
+const custom = [validator, "Number must be approporiate (min 8) length and have a '-' after 2 or 3 numbers"]
+
+const noteSchema = new mongoose.Schema({
+    name: {type: String, minlength: 3, required: true},
+    number: {type: String, validate: custom, required: true}
+})
 
 
 noteSchema.set('toJSON', {
